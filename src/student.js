a450cea047e5b66299ac077921cde46f3442c80e
km4891 <kmorarity@gmail.com>
2021-06-08 18:54:21 -0700

    working lesson 2

--- src/student.js
+++ src/student.js
@@ -1,8 +1,14 @@
-
+var myName = "William";
+var classDescription = "Computer Science 190";
+var theDate = "6/8/2021";
 
 function confirmlink(linkname) {
     if (confirm("Are you sure you want to jump to " + linkname + "?"))
          location = linkname;
  }
 
+ function displayInfo() {
+    if (alert(myName + classDescription + theDate))
+         location = displayInfo;
+ }
  
