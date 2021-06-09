0ae6ddd1d8fc9f469112df58f86272c1312453e0
km4891 <kmorarity@gmail.com>
2021-06-08 22:14:16 -0700

    lesson 2 done

--- src/student.js
+++ src/student.js
@@ -1,6 +1,9 @@
 var myName = "William";
 var classDescription = "Computer Science 190";
 var theDate = "6/8/2021";
+var firstname = "Billy";
+var l_name = "Bob"
+var fullName = firstname + l_name;
 
 function confirmlink(linkname) {
     if (confirm("Are you sure you want to jump to " + linkname + "?"))
@@ -8,7 +11,8 @@ function confirmlink(linkname) {
  }
 
  function displayInfo() {
-    if (alert(myName + classDescription + theDate))
+    if (alert(myName + "\n" + classDescription + "\n" + theDate))
          location = displayInfo;
  }
  
+ console.log(fullName);
