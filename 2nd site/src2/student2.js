20ec35fa8c1238846e9492a6ea75576e63f293fa
km4891 <kmorarity@gmail.com>
2021-07-01 22:51:28 -0700

    still playing around

--- 2nd site/src2/student2.js
+++ 2nd site/src2/student2.js
@@ -1,13 +1,15 @@
 var busName = "D*A*L*E's Army Surplus"
 
 function getHeader () {
-    var data = busName;
+    
+    var data = "<h1 style='padding-right:55%'>" + busName + "<h1>";
     document.getElementById('header').innerHTML = data;
 }
 
 function getFooter () {
-    var data = "<table style='width:100%'><tr style='background-color:#4b5320'><td >";
-    data +="<h1 style='text-align:right, text-size:20px'>" + busName + "</h1>";
-    data +="</td></tr></table>";
+    // var data = "<table style='width:100%'><tr style='background-color:#4b5320'><td >";
+    // data +="<h1 style='text-align:right, text-size:20px'>" + busName + "</h1>";
+    // data +="</td></tr></table>";
+
     return(data);
 }
