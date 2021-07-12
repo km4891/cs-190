3d55def95b61d583687876151189393e1663de53
km4891 <kmorarity@gmail.com>
2021-07-12 16:55:23 -0700

    working on main

--- 2nd site/src2/student2.js
+++ 2nd site/src2/student2.js
@@ -23,5 +23,29 @@ function getFooter (busName, address, phone) {
 document.getElementById("footer").innerHTML = data;
 }
 
+function makeMenu() {
+    // var size = product_list.length;
+    var data = ``;
+   
+    
+    for (var i = 0; i < 4; i++) {
+      data += "Menu <br>";
+    }
+    return data;
+  }
+
+  function makeMain () {
+      data = `<table style='width:100%' border=1 cellpadding=10><tr>
+      <td rowspan=3 style='width:30%;text-align:center;vertical-align:middle'>
+      <span id='prod_img'>Product Image</span></td>
+      <td id='prod_name'>Product Name</td>
+      <td style='text-align:right' id='prod_id'>Product ID</td></tr>
+      <tr><td style='text-align:center' colspan=2>
+      <span id='prod_bar'>Shopping Cart Link Bar</span> </td></tr>
+      <tr><td colspan=2 id='prod_desc'>Product Description</td></tr>
+      </table>`;
+      return data;
+  }
+
 
     
